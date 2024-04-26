bf35070b3dbe1a0bb0002e9229d71edf9bbd0c3f
gminnimk <165118770+gminnimk@users.noreply.github.com>
2024-04-26 10:34:11 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,9 +1,18 @@
 class Solution {
-    public int solution(int num1, int num2) {
-        if (num1 == num2){
-            return 1;
-        }else {
-            return -1;
-        }       
+    public static int solution(int age) {
+        int yearOfBirth = 2022 - age;
+        return yearOfBirth;
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        int age1 = 40;
+        int age2 = 23;
+
+        int result1 = solution(age1);
+        int result2 = solution(age2);
+
+        System.out.printf("2022년 기준 %d살이므로 %d년생입니다.\n", age1, result1);
+        System.out.printf("2022년 기준 %d살이므로 %d년생입니다.\n", age2, result2);
     }
 }
