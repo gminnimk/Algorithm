a9f8932349aef6e51c551ba4558234fcef1eb9c0
gminnimk <165118770+gminnimk@users.noreply.github.com>
2024-05-03 11:39:26 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,11 +1,19 @@
 class Solution {
     public String solution(int num) {
-        String answer = "";
+        // 정수 num이 짝수일 경우 "Even"을 반환!! => 이 경우 String으로 먼저 선언을 해줘야 함. 
         
-        if (num % 2 == 0){
-            return answer = "Even";
-        } else {
-            return answer = "Odd";
+        // 반환할 값을 선언하기 
+        
+        String input = ""; 
+        
+        
+        // if문으로 조건 값 찾기 
+        
+        if (num % 2 == 0){ // 짝수인 경우 
+            return input = "Even";
+        } else { // 홀수인 경우 
+            return input = "Odd";
         }
+        
     }
 }
