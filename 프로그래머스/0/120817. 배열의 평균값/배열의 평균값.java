1645a62546455a8c2b8ac81628e331b039a75022
gminnimk <165118770+gminnimk@users.noreply.github.com>
2024-05-10 12:25:07 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -1,20 +1,11 @@
 class Solution {
     public double solution(int[] numbers) { // 정수 배열 numbers가 매개변수로 주어짐. 
+        double answer = 0; // numbers의 원소의 평균값을 return 받을 변수
         
-        // numbers의 원소의 평균값을 return 하도록 구현! 
-        
-        // for문을 활용하여 배열에 모든 값들을 해당길이까지 더하여 리턴하도록 저장한 후에 
-        // 길이만큼 다시 나누어서 리턴 하면 원소의 평균값을 구할 수 있음! 
-        double answer = 0; 
-        
-        for(int i=0; i<numbers.length; i++){
-            answer += numbers[i]; 
-        } 
-        
-        answer = answer / numbers.length;
-        
-        return answer; 
-         
-
+        for(int i=0; i<numbers.length; i++){ // numbers 배열의 길이만큼 반복문이 수행이 되게
+            answer += numbers[i];   // answer라는 변수에 배열 numbers의 i번째 배열을 반복하여 저장
+        }
+        answer = answer/numbers.length; // answer 변수에 위 반복문에서 수행한 answer에 배열 numbers의 길이만큼 나눠줌 
+        return answer; // answer 최종 반환 
     }
 }
