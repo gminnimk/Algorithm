97b11c35aa5bd44289ff06e655f473a01b1ae63f
gminnimk <165118770+gminnimk@users.noreply.github.com>
2024-05-23 09:38:17 +0900

    [level 1] Title: 가장 비싼 상품 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/131697. 가장 비싼 상품 구하기/가장 비싼 상품 구하기.sql
+++ 프로그래머스/1/131697. 가장 비싼 상품 구하기/가장 비싼 상품 구하기.sql
@@ -1,2 +1,5 @@
-SELECT MAX(PRICE) AS MAX_PRCIE
-FROM PRODUCT 
+SELECT MAX(PRICE) AS MAX_PRICE FROM PRODUCT;
+
+-- PRODUCT 테이블의 PRICE 열에서 최대 값을 검색하고, 결과를 MAX_PRICE라는 별칭으로 변환. 
+
+-- 즉, 모든 제품 중에서 가장 높은 가격을 찾는 쿼리. 
