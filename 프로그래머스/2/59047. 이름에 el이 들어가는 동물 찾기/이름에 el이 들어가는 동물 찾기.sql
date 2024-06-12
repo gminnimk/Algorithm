913b2a29773e667343b414c4c5dbe245657d272e
gminnimk <165118770+gminnimk@users.noreply.github.com>
2024-06-13 01:28:25 +0900

    [level 2] Title: 이름에 el이 들어가는 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
+++ 프로그래머스/2/59047. 이름에 el이 들어가는 동물 찾기/이름에 el이 들어가는 동물 찾기.sql
@@ -1,2 +1,12 @@
--- 코드를 입력하세요
-SELECT
+-- ANIMAL_INS 테이블에서 ANIMAL_ID와 NAME 컬럼을 선택
+SELECT ANIMAL_ID, NAME 
+FROM ANIMAL_INS 
+
+-- ANIMAL_TYPE이 'Dog'인 행만 선택
+WHERE ANIMAL_TYPE = "Dog" 
+
+-- NAME 컬럼에 'EL'이 포함된 행만 선택
+AND NAME LIKE "%EL%" 
+
+-- 결과를 NAME 컬럼을 기준으로 오름차순으로 정렬
+ORDER BY NAME ASC;
