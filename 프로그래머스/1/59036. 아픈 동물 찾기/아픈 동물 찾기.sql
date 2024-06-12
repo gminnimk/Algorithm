a124293686693d5c796b096614837318e52ab738
gminnimk <165118770+gminnimk@users.noreply.github.com>
2024-06-13 01:27:09 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME -- 동물의 아이디 와 이름을 가져옴 
+FROM ANIMAL_INS -- ANIMAL_INS 테이블에서 
+WHERE INTAKE_CONDITION = 'Sick' -- 동물의 상태가 'Sick' 일떄
+ORDER BY ANIMAL_ID; -- 동물의 아이디를 기준으로 오름차순 정렬 
