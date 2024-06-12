c44fa05ba93ce7a4e3d2302101d1eeda815ed335
gminnimk <165118770+gminnimk@users.noreply.github.com>
2024-06-13 01:30:48 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -6,6 +6,3 @@ SELECT ANIMAL_ID, -- 동물의 아이디 컬럼 가져오기
        END AS '중성화' -- CASE 문을 사용해서 성별 및 중성화 여부 컬럼에 'Neutered' 또는 'Spayed' 단어가 들어있다면 O 출력 아닐시 X 출력 / 컬럼 이름은 "중성화"로 설정 
 FROM ANIMAL_INS
 ORDER BY ANIMAL_ID;
-
-
-
