cb640f7f6519615cba518d4af9ac87872a2b8a00
gminnimk <tvt1245@naver.com>
2026-01-26 15:53:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 107.10 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
 
 ### 성능 요약
 
-메모리: 96.5 MB, 시간: 87.27 ms
+메모리: 96.6 MB, 시간: 107.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 26일 14:13:02
+2026년 01월 26일 15:53:50
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,20 +3,22 @@ import java.util.HashMap;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
+        // 완주하지 못 한 선수의 이름을 리턴
+        // 참가자에서 완주자를 빼고 남은 값을 리턴
+        
         HashMap<String, Integer> hm = new HashMap<>();
         
-        // 참가자 중에 동명이인이 있을 수 있음
-        // 1. 참가자들 배열에 삽입
+        // 1. 참가자 정보 먼저 주입
         for (String player : participant) {
             hm.put(player, hm.getOrDefault(player, 0) + 1);
         }
         
-        // 2. 완주자 목록 기반 제거
+        // 2. 참가자에서 완주자 제거
         for (String player : completion) {
             hm.put(player, hm.get(player) - 1);
         }
         
-        // 3. value 가 0이 아닌 사람
+        // 3. 제거를 완료 했으니 이제 map 에서 남은 value 값 추출
         for (String key : hm.keySet()) {
             if (hm.get(key) != 0) {
                 answer = key;
