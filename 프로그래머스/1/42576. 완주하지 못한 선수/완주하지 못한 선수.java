15377568b177f6f0362809552972d184d341b536
gminnimk <tvt1245@naver.com>
2026-01-26 14:13:03 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 87.27 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,18 +3,20 @@ import java.util.HashMap;
 class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        
         HashMap<String, Integer> hm = new HashMap<>();
         
+        // 참가자 중에 동명이인이 있을 수 있음
+        // 1. 참가자들 배열에 삽입
         for (String player : participant) {
             hm.put(player, hm.getOrDefault(player, 0) + 1);
         }
         
-        // 완주자 명단 빼주기
+        // 2. 완주자 목록 기반 제거
         for (String player : completion) {
             hm.put(player, hm.get(player) - 1);
         }
         
+        // 3. value 가 0이 아닌 사람
         for (String key : hm.keySet()) {
             if (hm.get(key) != 0) {
                 answer = key;
