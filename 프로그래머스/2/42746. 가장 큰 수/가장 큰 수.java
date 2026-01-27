0479e659090f10854a6e1795e1dec144d84a226a
gminnimk <tvt1245@naver.com>
2026-01-27 18:11:13 +0900

    [level 2] Title: 가장 큰 수, Time: 171.45 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,26 +2,22 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        
-        // 1. 숫자를 문자열로 반환하여 배열에 담기
-        String[] strNumbers = new String[numbers.length];
+        String[] strArr = new String[numbers.length];
+        // 문자 형태로 모두 삽입
+        // ex) ["6", "10", "2" ]
         for (int i = 0; i < numbers.length; i++) {
-            strNumbers[i] = String.valueOf(numbers[i]);
+            strArr[i] = String.valueOf(numbers[i]);
         }
         
-        // 2. 커스텀 정렬: (b+a)와 (a+b)를 비교하여 내림차순 정렬
-        Arrays.sort(strNumbers, new Comparator<String>() {
-            @Override
-            public int compare(String a, String b) {
-                return (b + a).compareTo(a + b);
-            }
-        });
+        // 이제 정렬을 통해서 문자열의 조합을 만들고 내림차순 진행
+        Arrays.sort(strArr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
         
-        if (strNumbers[0].equals("0")) return "0";
+        // 예외 로직
+        if (strArr[0].equals("0")) return "0";
         
         StringBuilder sb = new StringBuilder();
-        for (String s : strNumbers) {
-            sb.append(s);
+        for (int i = 0; i < numbers.length; i++) {
+            sb.append(strArr[i]);
         }
         
         return sb.toString();
