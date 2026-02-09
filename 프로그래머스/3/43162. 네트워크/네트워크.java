5db53ac8a1e733281fdee3b038cfb72050755641
gminnimk <tvt1245@naver.com>
2026-02-09 21:28:07 +0900

    [level 3] Title: 네트워크, Time: 0.26 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,51 +1,59 @@
 class Solution {
     public int solution(int n, int[][] computers) {
-        int answer = 0;
         
         // [문제 분석]
         
-        // (1). A -> B -> C 일때 A -> C 도 간접적으로 연결 가능
+        // 컴퓨터 A -> B -> C 일 때 A 와 C도 간접적으로 연결되어 정보 교환 가능
+        // => 이러한 지문일때는 보통 그래프 문제 (DFS, BFS에서 DFS를 이용해서 풀기)
+        
         // n: 컴퓨터의 개수
         // computers: 연결에 대한 정보가 담긴 2차원 배열
-        // ex) [[1, 1, 0], [1, 1, 0], [0, 0, 1]]: return 2
         
-        // 연결을 확인하면서 탐색하는 과정: dfs
+        // ex)
+        // n: 3
+        // computers: [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+        // 첫번째 컴퓨터에는 자기 자신과 두번째 컴퓨터에 대한 연결 정보가 존재
+        // 두번째 컴퓨터에는 자기 자신과 첫번째 컴퓨터에 대한 연결 정보가 존재
+        // => 서로 연결되어 있으니 네트워크는 1
         
-        // 어떻게 확인을 할건데? 
-        // (1). n개의 컴퓨터를 탐색을 한다.
-        // (2). 탐색과 동시에 네트워크의 개수를 추가 및 갱신하고 해당 컴퓨터의 네트워크를 깊이 우선 탐색을 한다
-        // (3). 해당 컴퓨터의 연결 정보가 들어있는 네트워크 정보를 탐색하면서 다른 컴퓨터의 네트워크가 확인이 된다면 dfs를 재귀 호출하고 아니면 각 컴퓨터의 탐색(반복문)으로 돌아온다.
-        // (4). 이 과정을 컴퓨터의 개수가 끝날 때 까지 반복한다.
+        // 세번째 컴퓨터에는 자기 자신만 연결되어 있으니 네트워크 1
+        // return 2
         
-        boolean[] visited = new boolean[n]; // 해당 컴퓨터를 확인했는지 체크하기 위함
+        // [분석 결과]
+        // {1}. n개의 컴퓨터를 먼저 탐색을 시작한다.
+        // (이떄 방문기록이 없으면 네트워크 개수를 1 추가한다)
+        // {2}. 탐색을 하면서 해당 컴퓨터 방문 기록이 없다면 해당 컴퓨터 방문을 기록한다.
+        // {3}. 다른 컴퓨터에 대한 연결 정보가 있으면 해당 컴퓨터로 이동해서 방문을 기록한다.
+        // {4}. 연결 정보가 없다면 다시 돌아와서 반복문으로 방문기록이 없는 컴퓨터를 탐색한다.
+        
+        // {1}
+        int answer = 0;
+        boolean[] visited = new boolean[n]; // n개의 컴퓨터 방문 기록 일지
         
         for (int i = 0; i < n; i++) {
-            // 탐색 시작 (단, 방문 기록이 없다면)
             if (visited[i] != true) {
-                answer++; // 네트워크 추가
+                answer++;
                 dfs(i, n, computers, visited);
             }
         }
         
         // 네트워크의 개수를 return
-       return answer;
+        
+        // [제한사항]
+        // (1). i번 컴퓨터와 j번 컴퓨터가 연결되어 있으면 computers[i][j]를 1로 표현
+        // (2). computers[i][i]는 항상 1
+    
+        return answer;
     }
     
-    private void dfs(int current, int n, int[][] computers, boolean[] visited) {
-        visited[current] = true; // 해당 컴퓨터는 방문을 했음
+    private void dfs(int i, int n, int[][] computers, boolean[] visited) {
+        visited[i] = true; // {2}. 방문 기록 true
         
-        for (int next = 0; next < computers.length; next++) {
-            // 해당 컴퓨터 연결정보 내에 다른 컴퓨터 연결에 대한 정보가 있으면?
-            // 재귀를 호출해서 방문 true 로 변경
-            
-            // [제한사항]
-            // (0). [i][j] 가 동일한 값이면 무조건 1이므로 제외
-            // (1). i번 컴퓨터와 j번 컴퓨터가 연결되어 있으면 computers[i][j]를 1로 표현
-        
-            if (next != current && computers[current][next] == 1 && !visited[next]) {
-                dfs(next, n, computers, visited);
+        // {3}. 탐색한 컴퓨터의 다른 컴퓨터에 대한 정보 확인
+        for (int j = 0; j < computers.length; j++) {
+            if (computers[i][j] == 1 && visited[j] != true) {
+                dfs(j, n, computers, visited);
             }
         }
-        
     }
 }
