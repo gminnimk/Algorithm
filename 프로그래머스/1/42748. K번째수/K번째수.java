cf87b05bf22053f08b86b54e08343119644d8353
gminnimk <tvt1245@naver.com>
2026-02-10 15:33:13 +0900

    [level 1] Title: K번째수, Time: 0.50 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -4,15 +4,25 @@ class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         
-        for (int c = 0; c < commands.length; c++) {
-            int i = commands[c][0];
-            int j = commands[c][1];
-            int k = commands[c][2];
+        // [문제 분석]
         
-            int[] temp = Arrays.copyOfRange(array, i - 1, j);
-            Arrays.sort(temp);
-
-            answer[c] = temp[k - 1];
+        // 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬: ArrayRangeOf, Arrays.sort
+        
+        for (int n = 0; n < commands.length; n++) {
+            int i = commands[n][0];
+            int j = commands[n][1];
+            int k = commands[n][2];
+            
+            // i, j, k 를 지정했으니
+            // (1). 자르고
+            int[] slicedArray = Arrays.copyOfRange(array, i - 1, j);
+            
+            // (2). 정렬
+            Arrays.sort(slicedArray);
+            
+            // (3). 리턴 할 배열에 추가
+            answer[n] = slicedArray[k - 1];
+            
         }
         
         return answer;
