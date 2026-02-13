b6b89b086a1f9fc4bb9bbc7a79bdf5aa9e4498e2
gminnimk <tvt1245@naver.com>
2026-02-13 17:18:22 +0900

    [level 1] Title: 모의고사, Time: 0.98 ms, Memory: 94.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,37 +1,42 @@
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] answers) {
+    public int[] solution(int[] answers) {        
         // [문제 분석]
-        // 각 수포자가 찍는 방식: 반복 패턴 정리
+        
+        // 1번 수포자, 2번 수포자, 3번 수포자
+        // => 수포자 찍는 반복 패턴 기록 필요
         int[] p1 = {1, 2, 3, 4, 5};
         int[] p2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] p3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        // 가장 높은 점수 받은 사람을 알아야 하므로
-        int[] scores = new int[3];
+        // 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어 졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 함수 작성
+        // (1). 가장 많은 문제를 맞힌 사람이 누구인지 알기 위해: scores[] 배열
+        int[] scores = new int[3]; // 수포자가 3명이므로
         
-        // 실제 답변과 수포자의 답안을 비교하여 점수판 최신화
+        // 문제 맞힌 갯수 계산 시작
         for (int i = 0; i < answers.length; i++) {
             if (answers[i] == p1[i % p1.length]) { scores[0]++; }
             if (answers[i] == p2[i % p2.length]) { scores[1]++; }
-            if (answers[i] == p3[i % p3.length]) { scores[2]++; }
+            if (answers[i] == p3[i % p3.length]) { scores[2]++; }            
         }
         
-        // 점수판 기록이 끝나면 최대값 구하기
-        int max = Math.max(scores[0], Math.max(scores[1], scores[2]));
+        // 이후 최대 값 추출
+        int maxScore = Math.max(scores[0], Math.max(scores[1], scores[2]));
         
-        // 최대값 구했으니 가장 높은 점수를 받은 사람을 반환
+        // [제한 조건]
+        // 가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬
+        // (2). 여럿일 경우 사람의 수를 예측할 수 없기에 동적 list를 이용
         List<Integer> list = new ArrayList<>();
-        if (scores[0] == max)  { list.add(1); }
-        if (scores[1] == max)  { list.add(2); }
-        if (scores[2] == max)  { list.add(3); }
+        if (maxScore == scores[0]) { list.add(1); }
+        if (maxScore == scores[1]) { list.add(2); }
+        if (maxScore == scores[2]) { list.add(3); }
         
-        // list가 아닌 배열 형태로 반환 해야 하므로
         int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
+        // (3). 반환은 배열로 해야 하므로 다시 배열 형태로 변환 후 반환
+        for (int n = 0; n < list.size(); n++) {
+            answer[n] = list.get(n);
+        }    
         
         return answer;
     }
