ad9ab5296525b2713b5053f37bd7ddf9814cfa08
gminnimk <tvt1245@naver.com>
2026-02-15 18:57:54 +0900

    [level 1] Title: 폰켓몬, Time: 7.43 ms, Memory: 94.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,27 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
         
-        // N 마리의 폰켓몬 중 N/2 마리를 가져가도 됨
-        // N/2 마리의 폰켓몬 중 최대한 다양한 종류의 값을 가지길 원함
+        // [문제 분석]
+        // N 마리의 폰켓몬 중 N/2 마리를 가져가야 함
+        // int maxSelect = nums.length/2;
         
-        // 최대로 가질 수 있는 종류는 결국 N/2 개수
-        int maxCnt = nums.length/2;
+        // 폰켓몬 종류에 따라 번호를 붙여 구문 => 같은 종류의 폰켓몬은 같은 번호를 갖고 있음
+        // HashMap
         
-        // 1. 먼저 key, value 형태로 저장
-        HashMap<Integer, Integer> hm = new HashMap<>();
+        // nums: N마리 폰켓몬의 종류 번호가 담긴 배열
+        // 가장 많은 종류의 폰켓몬을 선택하는 방법?
+        // 예를 들어 6마리가 주어졌으면 
+        // int maxSelect = nums.length/2; == 3;
+        // 근데 최대 종류가 2종류라면
+        // int maxSpecies = map.size();
+    
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
         for (int mob : nums) {
-            hm.put(mob, hm.getOrDefault(mob, 0) + 1);
+            map.put(mob, map.getOrDefault(mob, 0) + 1);
         }
         
-        // 2. 여기서 중요한 건 종류 (size())
-        int speCnt = hm.size();
+        int maxSelect = nums.length/2;
+        int maxSpecies = map.size();
         
-        
-        // 3. 현재 저장된 종류와 최대로 가질 수 있는 종류 중 최소값 선택
-        // (3-1.) 만약 저장된 종류가 최대로 가질 수 있는 종류를 넘어버리면 maxCnt 선택
-        // (3-2.) 반대로 저장된 종류가 더 작으면 speCnt 선택
-        return Math.min(maxCnt, speCnt);
+        return Math.min(maxSelect, maxSpecies);
     }
 }
