6e4701e1eba27386fd51427a4ff2094e163e28be
gminnimk <tvt1245@naver.com>
2026-02-15 19:12:22 +0900

    [level 2] Title: 전화번호 목록, Time: 286.50 ms, Memory: 247 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,33 +2,34 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        // [제한 사항]
-        // 같은 전화번호가 중복해서 들어있지 않음: Map Or Set 을 사용
-        HashMap<String, Integer> map = new HashMap<>();
-        boolean answer = true;
-        
         // [문제 분석]
-        // 전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 확인
-        // : substring 사용
         
-        // (1). map에 전화번호를 모두 저장한 후 반복문을 통해 하나씩 꺼낸다
+        // phone_book: 전화번호부에 적힌 전화번호를 담은 배열
+        
+        // 전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우 false
+        // 아니면 true 반환
+        
+        // [제한 사항]
+        // phone_book의 길이는 1 이상 1,000,000 이하입니다.
+        // 같은 전화번호가 중복해서 들어있지는 않음
+        // => 해시맵
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        // 전화번호부 해시맵에 이관
         for (String number : phone_book) {
-            map.put(number, 1);
+            map.put(number, map.getOrDefault(number, 0) + 1);
         }
         
-        for (int i = 0; i < phone_book.length; i++) {
-            String currentNum = phone_book[i];
-            
-            // (2). 꺼낸 전화번호를 substring 으로 쪼꺤다
-            for (int j = 1; j < currentNum.length(); j++) {
-                String words = currentNum.substring(0, j);
-                // (3). 쪼개는 순간마다 map에 저장해둔 전화번호가 존재하는지 확인을 반복한다 (자기 자신은 제외하고)
-                if (map.containsKey(words)) {
+        // 전화번호부에서 접두어를 지정 후 검사 진행
+        for (String number : phone_book) {
+            for (int j = 1; j < number.length(); j++) {
+                if (map.containsKey(number.substring(0, j))) {
                     return false;
                 }
             }
         }
-    
-        return answer;
+        
+        return true;
     }
 }
