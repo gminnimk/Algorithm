3bfe7a82898092e974e4353301702ac5ca8c455d
gminnimk <tvt1245@naver.com>
2026-02-15 21:16:09 +0900

    [level 1] Title: K번째수, Time: 0.48 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,26 +3,19 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        
-        // [문제 분석]
-        
-        // 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬: ArrayRangeOf, Arrays.sort
+        // array: 배열
+        // commands: array, [i, j, k]를 원소로 가진 2차원 배열
         
         for (int n = 0; n < commands.length; n++) {
             int i = commands[n][0];
             int j = commands[n][1];
             int k = commands[n][2];
             
-            // i, j, k 를 지정했으니
-            // (1). 자르고
-            int[] slicedArray = Arrays.copyOfRange(array, i - 1, j);
+            int[] temp = Arrays.copyOfRange(array, i - 1, j);
             
-            // (2). 정렬
-            Arrays.sort(slicedArray);
+            Arrays.sort(temp);
             
-            // (3). 리턴 할 배열에 추가
-            answer[n] = slicedArray[k - 1];
-            
+            answer[n] = temp[k - 1];
         }
         
         return answer;
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
 
 ### 성능 요약
 
-메모리: 73.1 MB, 시간: 0.50 ms
+메모리: 77.5 MB, 시간: 0.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 10일 15:33:10
+2026년 02월 15일 21:16:07
 
 ### 문제 설명
 
