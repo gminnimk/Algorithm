e7172f1ccc7aa1cd50b2109b2af31642923d6729
gminnimk <tvt1245@naver.com>
2026-02-16 19:49:28 +0900

    [level 1] Title: K번째수, Time: 0.51 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,10 +2,20 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
-        // array: 배열
-        // commands: array, [i, j, k]를 원소로 가진 2차원 배열
         
+        int[] answer = new int[commands.length];
+        
+        // 지문 분석
+        
+        // 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬
+        // => int[] temp = Arrays.copyOfRange(자를 배열, 어디부터, 어디까지);
+        
+                
+        // k번째에 있는 수를 구한다
+        // => 자른 정렬에서
+        
+        // commands: 배열 array, [i, j, k]를 원소로 가진 2차원 배열
+        // 앞서 설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return
         for (int n = 0; n < commands.length; n++) {
             int i = commands[n][0];
             int j = commands[n][1];
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
 
 ### 성능 요약
 
-메모리: 77.5 MB, 시간: 0.48 ms
+메모리: 86.9 MB, 시간: 0.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 15일 21:16:07
+2026년 02월 16일 19:49:25
 
 ### 문제 설명
 
