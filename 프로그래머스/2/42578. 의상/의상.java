499bcbff694dcf1006a3b4a3fdb496db32eed6db
gminnimk <tvt1245@naver.com>
2026-02-16 17:44:02 +0900

    [level 2] Title: 의상, Time: 0.14 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,21 +3,20 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         
-        // [문제 분석]
-        // 이름과 종류 (String, String) => key, value (HashMap)
+        // 각 종류별 최대 1가지 의상만 착용 가능 => 카테고리별 분류
         HashMap<String, Integer> map = new HashMap<>();
-        
         for (String[] cloth : clothes) {
             String type = cloth[1];
-            map.put(type, map.getOrDefault(type, 0) + 1);
+            map.put(type, map.getOrDefault(type, 0) + 1); // 종류 별 저장
         }
         
-        int answer = 1; // (2). 하루에 최소 한 개의 의상은 입는다.
-        
-        for (String key : map.keySet()) {
-            answer *= (map.get(key) + 1);
+        // 하루에 최소 한 개의 의상은 입는다 => 안 입는 경우도 존재
+        int answer = 1;
+        for (int cloth : map.values()) {
+            answer *= cloth + 1; // 경우의 수 및 안입는 경우 최신화
         }
-        
-        return answer - 1; // 아무것도 입지 않은 경우 
+    
+        // 서로 다른 옷의 조합의 수 return (모든 조합의 수에서 아예 안 입는 수는 제거)    
+        return answer - 1;
     }
 }
