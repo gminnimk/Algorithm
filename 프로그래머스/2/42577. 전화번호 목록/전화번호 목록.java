3c693a69932359a548a73911189faa470a9c0b91
gminnimk <tvt1245@naver.com>
2026-02-23 21:59:23 +0900

    [level 2] Title: 전화번호 목록, Time: 286.44 ms, Memory: 207 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,39 +2,47 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        // [문제 분석]
         
-        // 전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우를 확인
+        // [문제 분석]
+        // 전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 겨우가 있는지 확인
         
         // phone_book: 전화번호부에 적힌 전화번호를 담은 배열
         // 어떤 번호가 다른 번호의 접두어인 경우 false
-        // 그렇지 않으면 true를 return
+        // 그렇지 않으면 true return
         
         
         // [제한 사항]
-        // phone_book의 길이는 1 이상 1,000,000 이하입니다.
+        // phone_book의 길이 1 ~ 1,000,000 이하
+        // => O (N^2) 시간 초과, O(N) 이 적합
         
-        // [코드 흐름]
-        // (1). 해시맵을 선언 후 전화번호 목록을 모두 이관
-        // (2). 반복문을 통해서 특정 번호를 기준으로 다른 번호와 요소를 비교한다
-        // => substring
+        // 같은 전화번호가 중복해서 들어있지 않습니다
+        // => HashSet
         
-        HashMap<String, Integer> map = new HashMap<>();
         
+        // [코드 흐름] 
+        // 코드 전체 시간 복잡도: O(N)
+        
+        // (1). 해시셋 선언 및 전화번호 배열 이관
+        // 해시(O(1)) & 1차 루프(O(N)): O(N)   
+        HashSet<String> set = new HashSet<>();
         for (String number : phone_book) {
-            map.put(number, 1);
+            set.add(number);
         }
         
-        for (String number : phone_book) {
-            // 전화번호부 목록에서 번호를 하나 꺼냈으니 해당 번호 요소 까지
-            for (int j = 1; j < number.length(); j++) {
-                if (map.containsKey(number.substring(0, j))) {
+        // (2). 이중 탐색 구조를 기반하여 전화번호부에 하나의 전화번호를 선택하고
+        // 해당 전화번호를 기준으로 다른 번호의 접두어가 충족되는지 검사한다
+        
+        // 루프(1,000,000) * 전화번호 길이(20): O(N)
+        for (String num : phone_book) {
+            String number = num;
+            // k가 1부터인 이유는 자기 자신을 검사에서 제외하기 위해
+            for (int k = 1; k < number.length(); k++) {
+                if (set.contains(number.substring(0, k))) {
                     return false;
-                };
+                }
             }
         }
         
-        // 접두어가 존재하지 않다면
         return true;
     }
 }
