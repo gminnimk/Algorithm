a6004c0bec2ddca2c7cc9c15b26faa4a96b23cda
gminnimk <tvt1245@naver.com>
2026-02-24 18:57:41 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 58.74 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 118 MB, 시간: 56.48 ms
+메모리: 112 MB, 시간: 58.74 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 27일 20:37:01
+2026년 02월 24일 18:57:38
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,18 +2,42 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        Stack<Integer> stack = new Stack<>();
         
+        // [문제 분석]
+        // arr: 숫자 0부터 9까지 이루어진 배열
+        // 이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거
+        // 제거 후 남은 수 반환할 때 배열 arr의 원소들의 순서를 유지해야 함
+        
+        // => Stack(LIFO) 또는 Queue(FIFO) 를 활용해서 연속적인 내용들은 제거해나가면 되지 않을까?
+        // 입출력 에시를 봤을때는 LIFO가 제일 적합하다고 판단
+        
+        // 배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return
+        
+        
+        // [제한 사항]
+        // 배열 arr의 크기 : 1,000,000 이하의 자연수
+        // => O(N^2) 미만의 시간복잡도로 해결
+        
+        // 배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수
+        
+        
+        // [코드 흐름]
+        // 전체 코드 시간 복잡도: O(N)
+        
+        // (1). LIFO 구조 Stack 선언 및 기존 배열 이관
+        Stack<Integer> stack = new Stack<>();
+        // 루프(O(N)) * 스택(O(1)): O(N)
         for (int num : arr) {
             if (stack.isEmpty() || stack.peek() != num) {
                 stack.push(num);
             }
         }
         
+        // (2). 스택에 담긴 결과를 배열로 변환
         int[] answer = new int[stack.size()];
-        StringBuilder sb = new StringBuilder();
+        // 루프(O(N)) * 스택(O(1)): O(N)
         for (int i = stack.size() - 1; i >= 0; i--) {
-            answer[i] = stack.pop();
+            answer[i] = stack.pop(); // 뒤에서부터 채워야 순서가 유지됨
         }
         
         return answer;
