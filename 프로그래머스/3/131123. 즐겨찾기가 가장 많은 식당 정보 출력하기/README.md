4597373827b2bf0d8c7a30b3d763f312da9da541
gminnimk <tvt1245@naver.com>
2026-03-02 17:52:47 +0900

    [level 3] Title: 즐겨찾기가 가장 많은 식당 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/README.md
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/README.md
@@ -1,6 +1,6 @@
 # [level 3] 즐겨찾기가 가장 많은 식당 정보 출력하기 - 131123 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131123?language=mysql) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131123) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@ Empty
 
 ### 제출 일자
 
-2026년 03월 01일 16:25:04
+2026년 03월 02일 17:52:45
 
 ### 문제 설명
 
--- 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
+++ 프로그래머스/3/131123. 즐겨찾기가 가장 많은 식당 정보 출력하기/즐겨찾기가 가장 많은 식당 정보 출력하기.sql
@@ -1,7 +1,10 @@
--- REST_INFO: 식당의 정보를 담은 테이블
-
+-- REST_INFO: 식당의 정보를 담은 테이
 -- 음식종류별로 즐겨찾기수가 가장 많은 식당의 음식 종류, ID, 식당 이름, 즐겨찾기수를 조회하는 SQL?
--- 결과는 음식 종류를 기준으로 내림차순 정렬
+-- => 음식종류별: GROUP BY FOOD_TYPE
+-- => 즐겨찾기수가 가장 많은: SELECT MAX(FAVORITES)
+-- => 음식 종류, ID, 식당 이름, 즐겨찾기수를 위의 조건을 충족해서 출력해야 하므로: 서브쿼리에 위 조건을 기입해야 함 (안 그러면 ID와 이름은 상관없는 값이 출력할 수 있기에)
+
+-- 결과는 음식 종류를 기준으로 내림차순 정렬: DESC
 
 SELECT
     FOOD_TYPE,
@@ -10,9 +13,8 @@ SELECT
     FAVORITES
 FROM
     REST_INFO
--- ID 와 식당 이름 또한 음식종류별 즐겨찾기가 제일 많은 곳이어야 하므로
--- 서브쿼리 조건 필요
 WHERE
+    -- 서브 쿼리 조건 삽입
     (FOOD_TYPE, FAVORITES) IN (
         SELECT
             FOOD_TYPE,
@@ -22,7 +24,5 @@ WHERE
         GROUP BY
             FOOD_TYPE
     )
-GROUP BY
-    FOOD_TYPE
 ORDER BY
     FOOD_TYPE DESC;
