2862273aa3373c9cc675caf6bdcd18aa13c7ece5
gminnimk <tvt1245@naver.com>
2026-03-03 17:59:14 +0900

    [level 2] Title: 카펫, Time: 0.12 ms, Memory: 96.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,50 +1,50 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int brown, int yellow) {
         
         // [문제 분석]
         // 중앙에는 노란색, 테두리 1줄은 갈색
-        // 각 색상 격자의 개수는 알지만, 전체 카펫의 크기는 기억 X
+        // 각 색칠된 격자의 개수는 기억했지만, 전체 카펫의 크기는 기억 X
         
-        // brown: 카펫의 갈색 격자의 수
-        // yellow: 카펫의 노란색 격자의 수
+        // brown: 갈색 격자의 수
+        // yellow: 노란색 격자의 수
         
-        // 카펫의 가로, 세로 크기를 순서대로 배열에 담아 return 
-        
-        // 주어진 조건에서 구할 수 있는 식
-        
-        // 카펫의 전체 가로를 w, 전체 세로를 h 라고 가정
-        // 전체 격자 개수 (w * h): brown + yellow
-        
-        // 노란색 격자 개수: 노란색 가로(w - 2), 세로(h - 2)
-        // => (w - 2) * (h - 2) = yellow
-        
-        // 갈색 격자 개수: 전체에서 노란색을 뺀 나머지
-        // => (w * h) - yellow = brown
-        
-        // 전체 카펫의 크기: brown + yellow;
+        // 카펫의 가로, 세로 크기를 순서대로 배열에 담아 return
         
         
-        // 노란색 가로 = 갈색 가로 - 2, 노란색 세로 = 갈색 세로 - 2;
-        // 카펫의 가로(w), 세로(h)의 식: (w - 2) + (h - 2): 
+        // < 구할 수 있는 식 >
+        
+        // (1). 카펫 전체 크기 = brown + yellow;
+        
+        // w를 카펫의 가로, h를 카펫의 세로라고 가정
+        // 중앙은 테두리의 가로, 세로보다 각각 -2씩 작음
+        // (2). yellow 크기 = ((w - 2) * (h - 2))
+        
+        
+        // [제한 사항]
+        // 카펫의 가로 길이는 세로 길이와 같거나, 세로 길이보다 깁니다.
+        // => h <= w
         
         
         // [코드 흐름]
-        int[] answer = new int[2];
-        int totalSize = brown + yellow; // 전체 격자 수
+        // 카펫의 가로, 세로 크기를 순서대로 배열에 담아 return
         
-        // 세로(h)는 최소 3부터 시작, 가로보다 작거나 같아야 하므로 sqrt(totalSize)
-        for (int h = 3; h <= Math.sqrt(totalSize); h++) {
-            if (totalSize % h == 0) {
-                int w = totalSize / h;
-            
-                // 핵심 조건 체크: 안쪽 노란색 격자 수가 yellow와 일치하는가?
+        // 카펫 전체 크기
+        int[] answer = new int[2];
+        int total = brown + yellow;
+        
+        // 반복문 순환을 높이(h) 기준으로 한다
+        for (int h = 3; h <= Math.sqrt(total); h++) {
+           
+            // 높이가 전체 크기의 약수이면
+            if (total % h == 0) {
+                int w = total / h; // 카펫의 가로
+                
+                // 카펫의 가로와 세로에서 각각 -2를 하고 곱한 값이 yellow, 즉 중앙인가?
                 if ((w - 2) * (h - 2) == yellow) {
                     answer[0] = w;
                     answer[1] = h;
                     break;
-                }
+                }   
             }
         }
     
