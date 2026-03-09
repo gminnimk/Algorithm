8c659b27762c6ac3b860eb393d9264ddc1539717
gminnimk <tvt1245@naver.com>
2026-03-09 18:48:53 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,19 +1,20 @@
--- ANIMAL_INS: 동물 보호소에 들어온 정보를 담은 테이블
+-- ANIMAL_INS: 동물 보호소에 들어온 동물의 정보를 담은 테이블
 -- ANIMAL_OUTS: 동물 보호소에서 입양 보낸 동물의 정보를 담은 테이블
 
--- 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름을 조회하는 SQL?
--- 결과는 보호 시작일이 빠른 순으로 조회
+-- 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름을 조회하는 SQL? 
+-- 이때 결과는 보호 시작일이 빠른 순으로 조회
 
 SELECT
     AO.ANIMAL_ID,
     AO.NAME
 FROM
     ANIMAL_OUTS AS AO
-LEFT JOIN 
+LEFT JOIN
     ANIMAL_INS AS AI
 ON
     AO.ANIMAL_ID = AI.ANIMAL_ID
 WHERE
-    AO.DATETIME < AI.DATETIME
+    AI.DATETIME > AO.DATETIME
 ORDER BY
     AI.DATETIME;
+    
