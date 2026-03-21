55b2a14216c8c3f52952438ab5a90e1a596d66fb
gminnimk <tvt1245@naver.com>
2026-03-21 14:03:49 +0900

    [level 2] Title: 타겟 넘버, Time: 4.65 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,40 +1,43 @@
 class Solution {
     
-    // 타겟넘버 횟수를 저장하기 위한 변수
     int count = 0;
     
     public int solution(int[] numbers, int target) {
-        // [문제 분석]
-        // n개의 음이 아닌 정수들이 존재
-        // 순서를 바꾸지 않고 '적절히 더하거나 빼서 타겟 넘버'를 만들기로 함.
-        // => DFS, 2개의 탐색 방향
         
-        // dfs 탐색을 하면서 필요한 매개변수가 무엇일까?
-        // 구하고자 하는 건 모든 배열의 조합을 끝까지 다 돌고 나서 타겟넘버가 되는 횟수임.
-        // 그렇다면 재귀를 통제하기 위한 변수와 타겟넘버를 확인하기 위한 변수가 필요하지 않을까.
-        dfs(numbers, target, 0, 0);
+        // [문제 분석]
+        // n개의 음이 아닌 정수들이 존재.
+        // 정수들 순서를 바꾸지 않고 적절히 더하거나 빼서 타겟 넘버를 만들려고 함.
+        // => DFS (깊이 우선 탐색은 순서를 바꾸지 않으며 백트리킹 개념으로 더하거나 빼서 탐색가능)
+        // 두가지의 경로 dfs(더하기), dfs(빼기) 재귀가 필요함
         
         // numbers: 사용할 수 있는 숫자가 담긴 배열
         // target: 타겟 넘버
-        // 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 
+        
+        // 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하시오
+        
+        // (1). dfs 탐색을 시작하기전 넘겨줘야 할 매개변수
+        // 재귀를 돌면서 계속 타겟넘버가 맞는지 확인해야 하니까 여태까지의 합을 저장하는 변수와
+        // 재귀를 탈출하기 위해 numbers 배열의 길이와 동일한지 검증하는 인덱스 변수
+        // numbers, target, sum, index
+        dfs(numbers, target, 0, 0);
         
         return count;
     }
     
-    public void dfs(int[] numbers, int target, int index, int sum) {
-        
-        // 조건문 (재귀 탈출, 배열을 끝까지 탐색했을 때)
-        if (index == numbers.length) {
-            // 여태까지의 조합의 합이나 빼기가 target가 일치하는가?
+    public void dfs(int[] numbers, int target, int sum, int index) {
+        // 재귀 탈출 조건문 
+        // 타겟 넘버가 맞는지는 모든 배열의 끝까지 탐색하고 검증 해야 함 (중간에 타겟넘버가 되었다고 검증 X)
+        if (numbers.length == index) {
             if (sum == target) {
                 count++;
             }
             
+            // 타겟 넘버가 아니면 이전 재귀로 돌아가기
             return;
         }
         
-        // 두 방향의 탐색
-        dfs(numbers, target, index + 1, sum + numbers[index]); // +
-        dfs(numbers, target, index + 1, sum - numbers[index]); // -
+        // 두 방향의 탐색 경로
+        dfs(numbers, target, sum + numbers[index], index + 1);
+        dfs(numbers, target, sum - numbers[index], index + 1);
     }
 }
