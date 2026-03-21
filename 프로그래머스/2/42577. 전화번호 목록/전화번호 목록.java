7db904d252c9e363d5539d0afcf328fb803bc461
gminnimk <tvt1245@naver.com>
2026-03-21 15:37:20 +0900

    [level 2] Title: 전화번호 목록, Time: 330.63 ms, Memory: 252 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,42 +2,31 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        
         // [문제 분석]
-        // 전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 겨우가 있는지 확인
-        
         // phone_book: 전화번호부에 적힌 전화번호를 담은 배열
-        // 어떤 번호가 다른 번호의 접두어인 경우 false
-        // 그렇지 않으면 true return
-        
+        // 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false
+        // 그렇지 않으면 true를 return
         
         // [제한 사항]
-        // phone_book의 길이 1 ~ 1,000,000 이하
-        // => O (N^2) 시간 초과, O(N) 이 적합
-        
-        // 같은 전화번호가 중복해서 들어있지 않습니다
-        // => HashSet
+        // phone_book: 1 ~ 1,000,000 이하: O(1)로 해결 필요
+        // 같은 전화번호가 중복해서 들어있지 않음: Set
         
         
-        // [코드 흐름] 
-        // 코드 전체 시간 복잡도: O(N)
-        
-        // (1). 해시셋 선언 및 전화번호 배열 이관
-        // 해시(O(1)) & 1차 루프(O(N)): O(N)   
+        // [코드 흐름]
+        // 전화번호부의 전화번호를 set에 전부 이관해서 중복 체크
         HashSet<String> set = new HashSet<>();
         for (String number : phone_book) {
             set.add(number);
         }
         
-        // (2). 이중 탐색 구조를 기반하여 전화번호부에 하나의 전화번호를 선택하고
-        // 해당 전화번호를 기준으로 다른 번호의 접두어가 충족되는지 검사한다
-        
-        // 루프(1,000,000) * 전화번호 길이(20): O(N)
-        for (String num : phone_book) {
-            String number = num;
-            // k가 1부터인 이유는 자기 자신을 검사에서 제외하기 위해
-            for (int k = 1; k < number.length(); k++) {
-                if (set.contains(number.substring(0, k))) {
+        // 이후 하나의 번호를 꺼낸 후 substring을 활용하여 일정 범위만큼 잘라서
+        for (int i = 0; i < phone_book.length; i++) {
+            String currentNum = phone_book[i];
+            
+            // substring을 활용해서 문자를 잘라야 하므로 1부터 시작 및 자기 자신은 제외
+            for (int j = 1; j < currentNum.length(); j++) {
+                // 기존 Set에 같은 문자열이 있는지 체크
+                if(set.contains(currentNum.substring(0, j))) {
                     return false;
                 }
             }
