899c931696b7a093bc6d561c54bf6355575767d8
gminnimk <tvt1245@naver.com>
2026-04-07 08:48:45 +0900

    [level 1] Title: 폰켓몬, Time: 7.39 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,32 +1,35 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
         
         // [문제 분석]
-        // N 마리의 폰켓몬 중 N/2 마리를 가져가도 됨
-        // 폰켓몬은 종류에 따라 번호를 붙여 구분: key, value => map
-        
-        // 최대한 많은 종류의 폰켓몬을 포함하여 N/2마리를 선택하려 함
-        // 폰켓몬 종류 번호의 개수를 return
-        // => map.size()
+        // N 마리의 폰켓몬 중 N/2마리를
+        // "폰켓몬은 종류에 따라 번호를 붙여 구분"
+        // => key, value 관리
         
         // nums: N마리 폰켓몬의 종류 번호가 담긴 배열
+        // 최대한 많은 종류의 폰켓몬을 포함해 N/2 마리를 선택.
+        // => 폰켓몬 종류 번호의 개수 return 
         
-        // (1). 해시 맵 선언 및 nums 이관
+        // Ex) 폰켓몬 개수는 8마리인데 종류가 2(5)가지 라면 2(4) return
+        
+        // 문제 정리:
+        // N 마리의 폰켓몬에서 N/2 마리의 폰켓몬을 선택하되 최대한 많은 종류를 가져오는 문제
+        // (HashMap, Math.min) 이용 해서 풀이
+        
+        
+        // [코드 흐름]
+        // (1). "폰켓몬은 종류에 따라 번호를 붙여 구분": 해시맵 이관
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int mob : nums) {
             map.put(mob, map.getOrDefault(mob, 0) + 1);
         }
         
-        // (2). 최대한 많은 종류를 포함해서 N/2 마리를 꺼내야 함
-        // Ex) nums에 6마리 및 2 종류의 폰켓몬이 존재하지만 N/2 = 3
-        // 그러나 최대 종류는 2 이므로 return 값은 2가 되어야 함
-        // 만약 종류가 선택의 개수 제한보다 많다고 해도 Math.min으로 return 을 조절
-        // Math.min(maxSpecies(2), maxSelect(3));
+        // (2). 최대한 많은 종류의 폰켓몬을 포함해 N/2 마리를 선택
         int maxSelect = nums.length/2;
         int maxSpecies = map.size();
         
-        return Math.min(maxSpecies, maxSelect);
+        return Math.min(maxSelect, maxSpecies);
     }
 }
