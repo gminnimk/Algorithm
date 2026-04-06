1706fbed3e39c77476f4f1092e1d69e5aef0e2c2
gminnimk <tvt1245@naver.com>
2026-04-06 20:01:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.92 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,42 +1,45 @@
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
         
         // [문제 분석]
-        // 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주함.
-        
+        // "단 한 명의 선수를 제외하고는 모든 선수가 마라톤에 완주"
         // participant: 마라톤에 참여한 선수들의 이름이 담긴 배열
         // completion: 완주한 선수들의 이름이 담긴 배열
         
-        // 완주하지 못한 선수의 이름을 return
+        // 완주하지 못한 선수의 이름을 return 
         
         
         // [제한 사항]
-        // 마라톤 경기에 참여한 선수의 수는 1명 이상 100,000명 이하입니다.: 해시 사용을 유추할 수 있음
-        // completion의 길이는 participant의 길이보다 1 작습니다.
-        // 참가자의 이름은 1개 이상 20개 이하의 알파벳 소문자로 이루어져 있습니다.
-        // 참가자 중에는 동명이인이 있을 수 있습니다.: key, value => map 사용
+        // 1명 이상 100,000명 이하 => O(N^2) X, O(1)
+        // 참가자 중에는 동명이인이 존재 => 중복 제거 X
+        
+        
+        // [문제 정리]
+        // 주어진 두 배열을 비교하여 하나의 배열에 없는 값을 return 하는 문제 (key 중복 허용)
+        // (입력값 크기를 고려해서 O(1), 중복 허용)
+        // => HashMap, (key, value)
         
         
         // [코드 흐름]
-        // (1). 해시 맵 선언 및 참가자 배열 이관
+        // (1). 주어진 참가자 배열을 HashMap 에 전부 이관한다.
         HashMap<String, Integer> map = new HashMap<>();
         for (String player : participant) {
             map.put(player, map.getOrDefault(player, 0) + 1);
         }
         
-        // (2). 맵과 완주자 배열 비교 및 중복되는 값 제거
+        // (2). 이관한 HashMap을 completion의 key의 value 값을 모두 차감한다.
         for (String player : completion) {
-            // map에 player 값이 있으면 - 1 처리
             map.put(player, map.get(player) - 1);
         }
         
-        // (3). 맵에 남은 key(완주하지 못한 선수의 이름) 값 return
+        // (3). 남은 key 값을 return 한다
         for (String key : map.keySet()) {
-            // 값이 0이 아닐때
+            
+            // map 에 value 값이 0이 아니라면 (해당 완주하지 못 한 참가자가 존재한다면)
             if (map.get(key) != 0) {
-                return key;
+                return key; // 어짜피 문제에서 1명이라고 했기에
             }
         }
         
