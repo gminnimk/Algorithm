fb8297ce1462491a48820428a2376f6a04012893
gminnimk <tvt1245@naver.com>
2026-04-08 14:25:57 +0900

    [level 2] Title: 전화번호 목록, Time: ., Memory: undefined -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,37 +1,43 @@
-import java.util.*;
+import java.util.HashSet;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        // [문제 분석]
-        // phone_book: 전화번호부에 적힌 전화번호를 담은 배열
-        // 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false
-        // 그렇지 않으면 true를 return
         
-        // [제한 사항]
-        // phone_book: 1 ~ 1,000,000 이하: O(1)로 해결 필요
-        // 같은 전화번호가 중복해서 들어있지 않음: Set
+        // [문제 분석]
+        // 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 존재?
+        // 같은 전화번호가 중복해서 들어있지 않음: 전화번호부를 중복 제거를 해주는 자료구조로 옮겨야 하지 않을까
+        
+        // phone_book: 전화번호를 담은 배열 (길이: 1 이상 ~ 1,000,000 이하) => O(1) 해결법 필요 판단
+        // 어떤 번호가 다른 번호의 접두어인 경우가 존재: false
+        // else: true
+        
+        
+        // [문제 정리]
+        // 해시 셋(중복 X) 내 전화번호부의 특정 전화번호를 꺼내고 다른 전화번호의 "접두어" 인 경우를 substring, contains를 활용해서 찾는 문제,
         
         
         // [코드 흐름]
-        // 전화번호부의 전화번호를 set에 전부 이관해서 중복 체크
+        boolean answer = true;
+        // (1). 중복 제거를 위한 해시 셋 선언 및 전화번호부 배열 이관 (제한 사항)
         HashSet<String> set = new HashSet<>();
         for (String number : phone_book) {
             set.add(number);
         }
         
-        // 이후 하나의 번호를 꺼낸 후 substring을 활용하여 일정 범위만큼 잘라서
+        // (2). 특정 전화번호를 꺼내서 전화번호부 내의 나머지 전화번호와 비교하여 접두어 찾기
         for (int i = 0; i < phone_book.length; i++) {
-            String currentNum = phone_book[i];
+            // 다른 전화번호와 비교하면서 찾는 과정에서는 본인 전화번호를 비교하면 안 됨
+            String current = phone_book[i]; // 현재 기준으로 잡은 전화번호
             
-            // substring을 활용해서 문자를 잘라야 하므로 1부터 시작 및 자기 자신은 제외
-            for (int j = 1; j < currentNum.length(); j++) {
-                // 기존 Set에 같은 문자열이 있는지 체크
-                if(set.contains(currentNum.substring(0, j))) {
+            // "접두어"만 확인하면 되니까
+            for (int j = 1; j < current.length(); j++) {
+                // 접두어 존재한다면? (본인은 제외)
+                if (set.contains(current.substring(0, j))) {
                     return false;
                 }
             }
         }
         
-        return true;
+        return answer;
     }
 }
