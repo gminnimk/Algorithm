29ec22c29bc2d9d4cae20fce8ae4997337f137f5
gminnimk <tvt1245@naver.com>
2026-04-10 20:20:55 +0900

    [level 1] Title: K번째수, Time: 1.33 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,43 +1,38 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         
         // [문제 분석]
-        // 배열 array의 i번째 숫자부터 j번째 숫자까지 자르고 정렬, k번째 수는?
-        // => 자르고 정렬 Arrays.copyOfRange(배열, i - 1, j), Arrays.sort()
+        // 배열을 i번째 ~ j번째까지 자르고 정렬 후 k번째에 있는 수?
+        // => Arrays.copyOfRange(대상 배열, 시작, 끝);
         
-        // commands: 배열 array, [i, j, k]를 원소로 가진 2차원 배열
+        // commands: [i, j, k]를 원소로 가진 2차원 배열
+        // 앞선 연산을 적용 했을 때 나온 결과를 배열에 담아 return
         
-        // 연산 적용 후 나온 결과를 배열에 담아 return
         
-        
-        // [제한 사항]
-        // array의 길이는 1 이상 100 이하입니다.
-        // array의 각 원소는 1 이상 100 이하입니다.
-        // commands의 길이는 1 이상 50 이하입니다.
-        // commands의 각 원소는 길이가 3입니다.
+        // [문제 정리]
+        // 주어진 범위(i, j)로 배열을 자르고 정렬하여 K번째 수를 반복적으로 구하는 문제 
         
         
         // [코드 흐름]
-        // 코드 전체 시간 복잡도: O(N logN)
         int[] answer = new int[commands.length];
         
-        // (1). 주어진 commands 배열을 반복문을 탐색하고 자르고 정렬한다
-        // 루프(O(N)) + 정렬(O(N logN)): O(N logN)
+        // (1). 반복해서 K번째 수를 구하고 배열에 저장해야 함
         for (int n = 0; n < commands.length; n++) {
+            // (2). 각 i,j,k 지정
             int i = commands[n][0];
             int j = commands[n][1];
             int k = commands[n][2];
-            
+        
+            // (3). 자르고 정렬
             int[] slicedArray = Arrays.copyOfRange(array, i - 1, j);
+            Arrays.sort(slicedArray);
             
-            Arrays.sort(slicedArray); // O(N logN)
-            
-            // (2). 배열에 k번째에 있는 수를 담는다
+            // (4). 배열에 추가
             answer[n] = slicedArray[k - 1];
         }
-
+        
         return answer;
     }
 }
