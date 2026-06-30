00cc2f56147bc096f0b2047e5c87f030ee5cd57c
gminnimk <tvt1245@naver.com>
2026-06-30 13:20:24 +0900

    [level 3] Title: 조건에 맞는 사용자와 총 거래금액 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++ 프로그래머스/3/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
@@ -1,15 +1,13 @@
--- USED_GOODS_BOARD: 중고 거래 게시판 정보 테이블
--- USED_GOODS_USER: 중고 거래 게시판 사용자 정보
+-- [문제]
 
+-- 중고 거래 게시판 정보를 담은 테이블: USED_GOODS_BOARD
+-- 중고 거래 게시판 사용자 정보를 담은 테이블: USED_GOODS_USER
 
--- [문제] USED_GOODS_BOARD와 USED_GOODS_USER 테이블에서 완료된 중고 거래의 총금액이 70만 원 이상인 사람의 회원 ID, 닉네임, 총거래금액을 조회하는 SQL문을 작성. 결과는 총거래금액을 기준으로 오름차순 정렬.
+-- USED_GOODS_BOARD와 USED_GOODS_USER 테이블에서 완료된 중고 거래의 총금액이 70만 원 이상인 사람의 회원 ID, 닉네임, 총거래금액을 조회하는 SQL문?
+-- '완료된 중고 거래의 총금액이 70만 원 이상' => WHERE STATUS = 'DONE' AND TOTAL_SALES > 700000
 
--- (1). '완료된 중고 거래의 총금액이 70만 원 이상인 사람'
--- => STATUS = DONE / SUM(PRICE) >= '700000'
-
-
--- (2). 총거래금액을 기준으로 오름차순 정렬
--- => ORDER BY TOTAL_SALES ASC
+-- 결과는 총거래금액을 기준으로 오름차순 정렬
+-- => ORDER BY ASC
 
 -- [풀이]
 SELECT
@@ -17,10 +15,14 @@ SELECT
     NICKNAME,
     SUM(PRICE) AS TOTAL_SALES
 FROM
-    USED_GOODS_BOARD
+    USED_GOODS_BOARD AS UGB
 JOIN
-    USED_GOODS_USER ON WRITER_ID = USER_ID
-WHERE STATUS = 'DONE'
-GROUP BY USER_ID
-HAVING SUM(PRICE) >= '700000'
-ORDER BY TOTAL_SALES;
+    USED_GOODS_USER AS UGU ON UGB.WRITER_ID = UGU.USER_ID
+WHERE
+    STATUS = 'DONE'
+GROUP BY
+    WRITER_ID
+HAVING
+    SUM(PRICE) >= 700000
+ORDER BY
+    TOTAL_SALES ASC;
