2476ce751051b57f6caee3374d1a8090bb94e65d
gminnimk <tvt1245@naver.com>
2026-09-03 09:09:04 +0900

    [level 2] Title: 상품 별 오프라인 매출 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
+++ 프로그래머스/2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
@@ -1,29 +1,21 @@
--- [ Step1: 제한 사항 확인 => 역분석 => 알고리즘 압축 ]
--- (1). PRODUCT 테이블과 OFFLINE_SALE 테이블에서 상품코드 별 매출액(판매가 * 판매량) 합계를 출력
--- => 두 테이블이 필연적으로 JOIN 이 되어야하며
--- => "상품코드 별" 이므로 GROUP BY PRODUCT_CODE
--- => "매출액(판매가 * 판매량) 합계" => SUM 집계 함수를 이용
+# [ 문제 ]
 
--- [ Step2: 핵심 명사/동사 키워드 추출 ]
--- 명사: PRODUCT, OFFLIME_SALE, 상품코드, 매출액(판매가 & 판매량)
--- 동사: 조인하다, 집계하다, 더하다(합계), 정렬하다
+-- - PRODUCT: 의류 쇼핑몰에서 판맨중인 상품들의 상품 점보를 담은 테이블 (PK: PRODUCT_ID)
+-- - OFFLINE_SALE: 테이블과 오프라인 상품 판매 정보를 담은 테이블 (FK: PRODUCT_ID)
+
+-- - PRODUCT 테이블과 OFFLINE_SALE 테이블에서 상품코드 별 매출액(판매가 * 판매량) 합계를 출력하는 SQL문을 작성해주세요. 결과는 매출액을 기준으로 내림차순 정렬해주시고 매출액이 같다면 상품코드를 기준으로 오름차순 정렬해주세요
 
 
--- [ 문제 ]
+# [ 메모레 멘탈 모델 ]
 
--- PRODUCT 테이블과 OFFLINE_SALE 테이블에서 상품코드 별 매출액(판매가 * 판매량) 합계를 출력하는 SQL문을 작성.
--- 결과는 매출액을 기준으로 내림차순 정렬해주시고 매출액이 같다면 상품코드를 기준으로 오름차순 정렬해주세요.
+-- - 1. 두 테이블을 PK 기준으로 이너 조인한다.
+-- - 2. PRODUCT_ID 기준으로 그룹화를 진행한다.
+-- - 3. (판매가 * SUM(판매량)) 합계를 SELECT 구문에서 구한다.
+-- - 4. 정렬 구체화.
 
--- PRODUCT: 판매중인 상품들의 상품 정보 테이블 (PK: PRODUCT_ID, UK: PRODUCT_CODE)
--- OFFLINE_SALE: 오프라인 상품 판매 정보 테이블 (PK: OFFLINE_SALE_ID, FK: PRODUCT_ID)
-
-
--- [ 풀이 ]
-
-SELECT PRODUCT_CODE,
-       SUM(P.PRICE * O.SALES_AMOUNT) AS SALES
+SELECT P.PRODUCT_CODE,
+       (P.PRICE * SUM(O.SALES_AMOUNT)) AS SALES
 FROM PRODUCT AS P
-JOIN OFFLINE_SALE AS O ON P.PRODUCT_ID = O.PRODUCT_ID
-GROUP BY P.PRODUCT_CODE
-ORDER BY SALES DESC,
-         P.PRODUCT_CODE ASC;
+INNER JOIN OFFLINE_SALE AS O ON P.PRODUCT_ID = O.PRODUCT_ID
+GROUP BY P.PRODUCT_ID
+ORDER BY SALES DESC, P.PRODUCT_CODE ASC;
