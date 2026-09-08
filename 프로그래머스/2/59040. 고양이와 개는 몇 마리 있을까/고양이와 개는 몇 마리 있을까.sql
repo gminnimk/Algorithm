3ea8ebb3cf7ffa7357482f70c0da44c0fde9d505
gminnimk <tvt1245@naver.com>
2026-09-08 13:59:49 +0900

    [level 2] Title: 고양이와 개는 몇 마리 있을까, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/2/59040. 고양이와 개는 몇 마리 있을까/고양이와 개는 몇 마리 있을까.sql
+++ 프로그래머스/2/59040. 고양이와 개는 몇 마리 있을까/고양이와 개는 몇 마리 있을까.sql
@@ -1,5 +1,15 @@
-SELECT ANIMAL_TYPE, -- 생물 종 컬럼을 가져옴
-       COUNT(ANIMAL_TYPE) AS COUNT  -- 생물 종의 숫자를 COUNT 로 표시 및 출력 
-FROM ANIMAL_INS -- ANIMAL_INS 테이블에서 가져옴
-GROUP BY ANIMAL_TYPE -- 생물 종을 그룹 별로
-ORDER BY ANIMAL_TYPE -- 생물 종을 오름차순으로 정렬 
+# [ 문제 ]
+
+-- - ANIMAL_INS: 동물 보호소에 들어온 동물의 정보를 담은 테이블 (PK: ANIMAL_ID)
+-- - 동물 보호소에 들어온 동물 중 고양이와 개가 각각 몇 마리인지 조회하는 SQL문을 작성해주세요. 이때 고양이를 개보다 먼저 조회해주세요.
+
+# [ 메모리 멘탈 모델 ]
+
+-- - 1. ANIMAL_TYPE 으로 그룹화 진행 및 COUNT 집계함수로 쿼리 출력
+-- - 2. 고양이가 먼저오도록 정렬 기준
+
+SELECT ANIMAL_TYPE,
+       COUNT(ANIMAL_ID) AS count
+FROM ANIMAL_INS
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE ASC;
